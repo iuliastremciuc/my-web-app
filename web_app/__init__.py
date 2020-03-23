219624318c344f604f5d433988dfd5036c7e2b5b
Iulia Stremciuc <iuliastremciuc@gmail.com>
2020-03-23 18:43:44 -0500

    generated development db

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -4,23 +4,21 @@
 
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app.db"
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////C:/Users/iulia/Desktop/web_app.db"
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
-    @app.route("/")
-    def hello():
-        print("VISITED THE HELL PAGE")
-        return "Hello World!"
 
-    @app.route("/about")
-    def about():
-        print("VISITED THE ABOUT PAGE")
-        return "About Me!!"
-    
     return app
 
 if __name__ == "__main__":
